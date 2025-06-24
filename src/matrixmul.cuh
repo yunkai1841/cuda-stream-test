7eef7d761513c1c4df7a0cd51f73cabbdf0a177c
Yunkai <70383706+yunkai1841@users.noreply.github.com>
2025-06-24 05:11:40 +0000

    fix: add missing #pragma once directive to matrixmul.cuh

--- src/matrixmul.cuh
+++ src/matrixmul.cuh
@@ -1,3 +1,5 @@
+#pragma once
+
 __global__ void matrixMulKernel(float *C, const float *A, const float *B, int N);
 __global__ void matrixMulTilingKernel(float *C, const float *A, const float *B, int N);
 __global__ void matrixMulTilingSharedKernel(float *C, const float *A, const float *B, int N);
